a5ec48f1fa966b984e8fbbd65396099c323aadc8
zhuweijian <2366750579@qq.com>
2017-09-25 07:53:54 +0000

    Add user statuses

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\User;
+// use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,15 +20,24 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         //
         // Model::reguard();
-        $users = factory(User::class)->times(50)->make();
-        User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
 
-        $user = User::find(1);
-        $user->name = 'Aufree';
-        $user->email = 'aufree@yousails.com';
-        $user->password = bcrypt('password');
-        $user->is_admin = true;
-        $user->activated = true;
-        $user->save();
+
+        // $users = factory(User::class)->times(50)->make();
+        // User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
+        //
+        // $user = User::find(1);
+        // $user->name = 'Aufree';
+        // $user->email = 'aufree@yousails.com';
+        // $user->password = bcrypt('password');
+        // $user->is_admin = true;
+        // $user->activated = true;
+        // $user->save();
+
+
+
+        Model::unguard();
+        $this->call(UsersTableSeeder::class);
+        $this->call(StatusesTableSeeder::class);
+        Model::reguard();
     }
 }
