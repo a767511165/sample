da22ce4d08094ac9d5703116502794499f14c5ef
zhuweijian <2366750579@qq.com>
2017-09-25 04:06:23 +0000

    Add password resets & email configuration

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Notifications\ResetPassword;
 
 class User extends Authenticatable
 {
@@ -36,17 +37,24 @@ class User extends Authenticatable
       });
     }
 
-/**
-*1.为 gravatar 方法传递的参数 size 指定了默认值 100；
-*2.通过 $this->attributes['email'] 获取到用户的邮箱；
-*3.使用 trim 方法剔除邮箱的前后空白内容；
-*4.用 strtolower 方法将邮箱转换为小写；
-*5.将小写的邮箱使用 md5 方法进行转码；
-*6.将转码后的邮箱与链接、尺寸拼接成完整的 URL 并返回；
-*/
+    /**
+    *1.为 gravatar 方法传递的参数 size 指定了默认值 100；
+    *2.通过 $this->attributes['email'] 获取到用户的邮箱；
+    *3.使用 trim 方法剔除邮箱的前后空白内容；
+    *4.用 strtolower 方法将邮箱转换为小写；
+    *5.将小写的邮箱使用 md5 方法进行转码；
+    *6.将转码后的邮箱与链接、尺寸拼接成完整的 URL 并返回；
+    */
     public function gravatar($size = '100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPassword($token));
+    }
+
+
 }
